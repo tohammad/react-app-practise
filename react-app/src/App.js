6f0100f11dc4156a9a90faa2bfbad8cfc56cad4e
Hammad Mustafa <tohammad@gmail.com>
2022-08-28 14:58:55 +0530

    function component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import ClassComponent from'./ClassComponent';
+import FunctionComponent from'./FunctionComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ClassComponent name="I'm class component"/>
+        <FunctionComponent name="I am a function component"/>
       </header>
       
     </div>
