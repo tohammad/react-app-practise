05579c30f7aaf3b203713f74debf63c666089403
Hammad Mustafa <tohammad@gmail.com>
2022-12-06 20:03:27 +0530

    prop drilling

--- react-app/src/Parent.js
+++ react-app/src/Parent.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Child1 from "./Child1";
 const Parent = () => {
-    const [name, setName] = useState("Hammad");
+    const [name, ] = useState("Hammad");
     return (
         <div>
         <Child1 name={name}/>
