e708ec7e1480b58f969ad9af60aed5ac7a2b0685
Hammad Mustafa <tohammad@gmail.com>
2023-01-06 14:49:09 +0530

    useId hook

--- react-app/src/MyComponent.js
+++ react-app/src/MyComponent.js
@@ -1,50 +1,15 @@
-import { useState, useEffect, useTransition } from "react";
+import { useId } from "react";
 const MyComponent = () => {
-  const [users, setUsers] = useState([]);
-  const [filteredUsers, setFilteredUsers] = useState([]);
-  const [searchText, setSearchText] = useState("");
-  const [isPending, startTransition] = useTransition();
-
-  const getUsers = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const usersRes = await response.json();
-    setUsers(usersRes);
-    setFilteredUsers(usersRes);
-  };
-
-  const handleSearch = (event) => {
-    startTransition(() => {
-      setSearchText(event.target.value);
-    });
-  };
-
-  useEffect(() => {
-    getUsers();
-  }, []);
-
-  useEffect(() => {
-    const filteredUsers = users.filter((user) =>
-      user.name.includes(searchText)
-    );
-    setFilteredUsers(filteredUsers);
-  }, [searchText]);
-
+  const id = useId();
   return (
     <>
-      <div className="row">
-        <div className="column">
-          <h2>Search User</h2>
-          <input type="text" placeholder="user name" onChange={handleSearch} />
-          {isPending && <p>Updating List...</p>}
-        </div>
+      <div>
+        <label htmlFor={`${id}-email`}>Email</label>
+        <input id={`${id}-email`} type="email" />
       </div>
-
-      <div class="listItems col-12">
-        <ul class="todo">
-          {filteredUsers.map((user, index) => {
-            return <li key={index}>{user.name}</li>;
-          })}
-        </ul>
+      <div>
+        <label htmlFor={`${id}-password`}>Password</label>
+        <input id={`${id}-password`} type="password" />
       </div>
     </>
   );
